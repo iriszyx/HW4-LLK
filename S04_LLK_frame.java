f77795a2572587cf6b3fda393b4903660fc2cdb5
iriszyx <zyx_iris@126.com>
2015-04-12 22:01:32 +0800

    Update S04_LLK_frame.java

--- S04_LLK_frame.java
+++ S04_LLK_frame.java
@@ -1,3 +1,4 @@
+package homework4;
 import java.awt.*;
 import java.math.*;
 import java.awt.event.*;
@@ -70,26 +71,49 @@ class Match{
 }
 public class S04_LLK_frame extends JFrame{
 	private Match match=new Match();
-	private JLabel admin_l=new JLabel("Administrator Control Panel:");
-	private JTextArea admin_dis=new JTextArea(11,40);
-	private JPanel admin_p=new JPanel();
-	private JButton admin_add=new JButton("Add New Books");
+	private Box admin_v= Box.createVerticalBox();
+	private JPanel admin_s0 =new JPanel();
+	private JPanel admin_e0 =new JPanel();
+	private JButton admin_start =new JButton("START");
+	private JButton admin_exit =new JButton("EXIT");
+	private JTextField admin_t = new JTextField("连 连 看");
+	private JTextField admin_name = new JTextField("name*4");
 	public S04_LLK_frame(){
 		setSize(1000,700);
 		setTitle("Lian Lian Kan");
 		setVisible(true);
-		admin_add.addActionListener(new ActionListener(){
+		admin_start.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
+				System.exit(0);
 			}
 		});
-		admin_p.add(admin_l);
-		admin_p.add(admin_add);
-		admin_p.add(admin_dis);
-		admin_dis.setLineWrap(true);
-		admin_dis.setEditable(false);
+		admin_exit.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+		});
+		admin_t.setFont(new Font("楷体",50,100)); //title
+		admin_t.setBorder(null);
+		admin_t.setEditable(false);
+		admin_t.setHorizontalAlignment(JTextField.CENTER);
+
+		admin_name.setFont(new Font("楷体",50,20)); //title
+		admin_name.setBorder(null);
+		admin_name.setEditable(false);
+		admin_name.setHorizontalAlignment(JTextField.CENTER);	
+		
+		admin_s0.add(admin_start);
+		admin_e0.add(admin_exit);
+		
+		admin_v.add(admin_t);
+		admin_v.add(admin_s0);
+		admin_v.add(admin_e0);
+		admin_v.add(admin_name);
+		
 		getContentPane().setLayout(new BorderLayout(5,5));
-		getContentPane().add("North",admin_p);
+		getContentPane().add(admin_v, BorderLayout.CENTER);
 	}
 	public static void main(String []args){
 		S04_LLK_frame gui=new S04_LLK_frame();
